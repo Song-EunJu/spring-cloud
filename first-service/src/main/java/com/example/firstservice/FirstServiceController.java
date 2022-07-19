4996ad3dab7e12a9bf8bb7bcb5dfb805d20cffce
Song-EunJu <jd06280@naver.com>
2022-07-19 16:56:07 +0900

    feat: User Service 기능 구현

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -2,15 +2,25 @@ package com.example.firstservice;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+    Environment env;
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service.";
@@ -23,7 +33,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. first service";
+    public String check(HttpServletRequest request){
+        log.info("Server port:{}", request.getServerPort());
+        return String.format(env.getProperty("local.server.port"));
     }
 }
